bc93180e2c7dc57191c483a0ee5a843a686e7c79
kkkkkxiaofei <xifzhang@thoughtworks.com>
2020-06-11 00:14:03 +0800

    test render

--- src/examples/example6/index.js
+++ src/examples/example6/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import Render from './render/render';
 
-const CustomRenderExample = () => {
+const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -8,7 +9,18 @@ const CustomRenderExample = () => {
       <button onClick={() => { setCount(count + 1); }}>click me</button>
       <span>{count}</span>
     </div>
-  );
+  );;
+};
+
+const CustomRenderExample = () => {
+  const start = () => {
+    const root = document.getElementById('root');
+    const newRoot = document.createElement('div')
+    root.replaceWith(newRoot);
+    Render.render(<Counter />, newRoot);
+  }
+
+  return <button onClick={() => start()}>click me to render</button>
 }
 
 export default CustomRenderExample;
