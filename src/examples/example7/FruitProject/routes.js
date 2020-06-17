99953e3658a1fd3763473c6529952f8445f0c5b5
kkkkkxiaofei <xifzhang@thoughtworks.com>
2020-06-17 23:30:39 +0800

    remove useless config

--- src/examples/example7/FruitProject/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {
-  Route,
- } from 'react-router-dom';
-import Fruits from './components/Fruits';
-
-export default ({ prefix = '' }) => (
-  <Route path={`${prefix}/fruits`} component={Fruits} />
-);
