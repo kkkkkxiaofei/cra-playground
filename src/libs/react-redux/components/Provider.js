b54544044fca13e7b03227384a8beb31e66c844d
Xiaofei ZHANG <xifzhang@thoughtworks.com>
2020-07-27 20:43:35 +0800

    introduce subscription

--- src/libs/react-redux/components/Provider.js
+++ src/libs/react-redux/components/Provider.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactReduxContext from './ReactReduxContext';
+import Subscription from '../utils/Subscription';
 
 const Provider = ({ store, context, children }) => {
   const Context = context || ReactReduxContext;
-  
+  const subscription = new Subscription(store);
+  const contextValue = {
+    store,
+    subscription
+  };
   return (
-    <Context.Provider value={{ store }}>
+    <Context.Provider value={contextValue}>
       {children}
     </Context.Provider>
   )
---
+++ src/libs/react-redux/utils/Subscription.js
@@ -0,0 +1,31 @@
+function Subscription(store, parentSub) {
+  const self = this;
+  self.store = store;
+  self.parentSub = parentSub;
+  self.unSubscribe = null;
+  self.listeners = [];
+  self.subscribe = function(listener) {
+    self.listeners.push(listener);
+    return () => self.listeners.splice(self.listeners.indexOf(listener), 1);
+  };
+  self.notify = function() {
+    self.listeners.forEach(listener => listener());
+  }
+  self.onStateChange = null;
+  self.handleStateChange = function() {
+    if (self.onStateChange) {
+      self.onStateChange();
+    }
+  }
+  self.trySubScribe = function() {
+    if (!self.unSubscribe) {
+      self.unSubscribe = parentSub ? 
+        self.parentSub.subscribe(self.handleStateChange) :
+        self.store.subscribe(self.handleStateChange);
+    }
+  }
+  
+  return self;
+}
+
+export default Subscription;
