aa652b95d24ae9249b5c17693d49c2296eaf64cc
kkkkkxiaofei <xifzhang@thoughtworks.com>
2020-09-23 00:18:00 +0800

    modify suspense demo

--- src/examples/example0/index.js
+++ src/examples/example0/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, Suspense } from 'react';
-import createResource from './api';
+import React, { useState, useEffect, Suspense, useMemo } from 'react';
+import createResource, { fetchCountry } from './api';
 
 const Country = props => {
   const country = props.resouce.read();
@@ -9,16 +9,44 @@ const Country = props => {
     </div>
   )
 }
-
 const SuspenseExample = () => {
-  const [resource, setResource] = useState(createResource(0));
+  return (
+    <>
+      <WithSuspense />
+      <NoSuspense />
+    </>
+  )
+}
+
+const WithSuspense = () => {
+  const [id, setId] = useState(0);
+  console.log('====WithSuspense====');
   return (
     <div>
       <p>The implementation of MySuspense can work under React 16.2, but you can not use it in latest(16.13) because the Suspense works perfectly;</p>
-      <button onClick={() => setResource(createResource(resource.id + 1))}>next</button>
+      <button onClick={() => setId(id + 1)}>next</button>
       <Suspense fallback={<div>C1ountry is loading1...</div>}>
-        <Country resouce={resource.countryResource} />
+        <Country resouce={createResource(id).countryResource} />
       </Suspense>
+    </div>
+  );
+}
+
+const NoSuspense = () => {
+  const [id, setId] = useState(0);
+  const [result, setResult] = useState('');
+  console.log('====NoSuspense====', id);
+  useEffect(() => {
+    fetchCountry(id).then(result => setResult(result));
+  }, [id]);
+  
+
+  const onClick = () => setId(id + 1);
+  return (
+    <div>
+      <p>click next very quickly to see the concurrency impact;</p>
+      <button onClick={onClick}>next</button>
+      <div>{result}</div>
     </div>
   );
 }
